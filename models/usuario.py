958de8ed03115b100f599fd601dd46148b894055
Tiago Sales <tiagosales.ti@gmail.com>
2023-11-21 23:04:54 -0300

    chave estrangeira favorito x usuario

--- models/usuario.py
+++ models/usuario.py
@@ -9,10 +9,14 @@ class Usuario(db.Model,UserMixin):
     senha = db.Column(db.String(255), nullable=False)
     id_perfil = db.Column(db.Integer, nullable=False)
     data_cadastro = db.Column(db.DateTime, nullable=False)
+    favoritos = db.relationship('Favorito', back_populates='usuario', cascade='all, delete-orphan')
+
 
 class Favorito(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    id_usuario = db.Column(db.Integer, nullable=False)
+    id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id', ondelete='CASCADE'), nullable=False)
     id_imovel = db.Column(db.Integer, db.ForeignKey('imovel.id', ondelete='CASCADE'), nullable=False)
     imovel = db.relationship('Imovel', back_populates='favoritos')
+    usuario = db.relationship('Usuario', back_populates='favoritos')
+
 
